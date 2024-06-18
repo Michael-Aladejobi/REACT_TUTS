2c3bd4d95d212323a67e70b4c69ceae4bc250976
mayorgt_ <mikkystechs@gmail.com>
2024-06-18 15:42:01 +0100

    created a couple of components with just jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-function Greeting() {
-    return <h2>My First Component</h2>;
+function BookList() {
+    return (
+        <section>
+            <Book />
+            <Book />
+            <Book />
+            <Book />
+        </section>
+    );
 }
 
+const Book = () => {
+    return (
+        <article>
+            <Image />
+            <Title />
+            <Author />
+        </article>
+    );
+};
+
+const Image = () => (
+    <img
+        src='https://m.media-amazon.com/images/I/41Wts36fLcL._AC_UL320_.jpg'
+        alt='Ghost Platoon'
+    />
+);
+const Title = () => <h2>Ghost Platoon </h2>;
+const Author = () => <h4>John C Campbell </h4>;
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<Greeting />);
+root.render(<BookList />);
