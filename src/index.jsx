a19b8083663787662806e4924cf5c3073c986da4
mayorgt_ <mikkystechs@gmail.com>
2024-06-19 09:10:28 +0100

    hovering comps, props, js destructuring

--- src/index.jsx
+++ src/index.jsx
@@ -1,35 +1,48 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
+
+const firstBook = {
+    author: "John A Campbell",
+    title: "Ghost Platoon",
+    img: "https://m.media-amazon.com/images/I/41Wts36fLcL._AC_UL320_.jpg",
+};
+
+const secondBook = {
+    author: "John B Campbell",
+    title: "Ghost Platoon",
+    img: "https://m.media-amazon.com/images/I/41Wts36fLcL._AC_UL320_.jpg",
+};
 
 function BookList() {
     return (
-        <section>
-            <Book />
-            <Book />
-            <Book />
-            <Book />
+        <section className='booklist'>
+            <Book
+                author={firstBook.author}
+                title={firstBook.title}
+                img={firstBook.img}
+            />
+            <Book
+                author={secondBook.author}
+                title={secondBook.title}
+                img={secondBook.img}
+            />
         </section>
     );
 }
 
-const Book = () => {
+const Book = (props) => {
+    const { img, title, author } = props;
+    console.log(props);
     return (
-        <article>
-            <Image />
-            <Title />
-            <Author />
+        <article className='book'>
+            <img src={img} alt={title} />
+            <h2>{title}</h2>
+            <h4>{author}</h4>
         </article>
     );
 };
 
-const Image = () => (
-    <img
-        src='https://m.media-amazon.com/images/I/41Wts36fLcL._AC_UL320_.jpg'
-        alt='Ghost Platoon'
-    />
-);
-const Title = () => <h2>Ghost Platoon </h2>;
-const Author = () => <h4>John C Campbell </h4>;
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(<BookList />);
