4fa68f6431ceac2fea48c3d6d70b585b777162aa
Michael-Aladejobi <mikkystechs@gmail.com>
2024-08-06 14:21:58 +0100

    ...

--- src/index.jsx
+++ src/index.jsx
@@ -1,37 +1,14 @@
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { Children } from "react";
-
-const books = [
-    {
-        author: "Jordan Moore",
-        title: "Interesting Facts For Curious Minds",
-        img: "https://images-na.ssl-images-amazon.com/images/I/71m+Qtq+HrL._AC_UL900_SR900,600_.jpg",
-        id: 1,
-    },
-    {
-        author: "James Clear",
-        title: "Atomic Habits",
-        img: "https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL900_SR900,600_.jpg",
-        id: 2,
-    },
-];
+import { books } from "./books";
 
 const BookList = () => {
-    const displayValue = () => {
-        console.log("shakeAndBake");
-    };
-
     return (
         <section className="booklist">
             {books.map((book, index) => {
                 return (
                     <>
-                        <Book
-                            {...book}
-                            key={index}
-                            displayValue={displayValue}
-                        />
+                        <Book {...book} key={index} />
                     </>
                 );
             })}
@@ -41,13 +18,13 @@ const BookList = () => {
 
 const Book = (props) => {
     // console.log(props);
-    const { author, title, img, displayValue } = props;
+    const { author, title, img } = props;
 
     return (
         <article className="book">
             <img src={img} alt="images" />
             <h2>{title}</h2>
-            <button onClick={displayValue}>click me</button>
+            <button>click me</button>
             <h4>{author}</h4>
         </article>
     );
